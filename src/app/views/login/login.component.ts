a6b094c9b5b8d94dfdc17a8657597b48466695b1
Daphne <remalidaphne@gmail.com>
2019-01-31 01:05:26 +0100

    fix navbar state to display login form

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -11,6 +11,15 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
+    this.closeNavbar();
+  }
+
+  closeNavbar() {
+    let navbar = document.getElementsByClassName('sidebar-fixed');
+    console.log(navbar);
+    if (navbar.length >= 0) {
+      navbar[0].classList.remove('sidebar-lg-show');
+    }
   }
 
 }
